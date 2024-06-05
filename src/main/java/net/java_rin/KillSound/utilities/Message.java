bea727bfb6435eaa8ac2a4a6e3d582321d075404
Rin <duggerseven@gmail.com>
2024-06-05 13:51:32 +0700

    add method spacing

--- src/main/java/net/java_rin/KillSound/utilities/Message.java
+++ src/main/java/net/java_rin/KillSound/utilities/Message.java
@@ -6,9 +6,11 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class Message {
+
     public static void send(CommandSender sender, String message) {
-        send(sender, message, "&a");
+        send(sender, message, "&f");
     }
+
     public static void send(CommandSender sender, String message, String prefix) {
         sender.sendMessage(ChatColor.translateAlternateColorCodes('&', prefix + message));
     }
